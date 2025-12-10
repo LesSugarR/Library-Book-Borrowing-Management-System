51e12f9b04f128f5e65b79d6d94552b507074287
LesSugarR <nuomi1007@126.com>
2025-12-10 11:56:06 +0800

    add some empty files again

--- include/Functions.h
+++ include/Functions.h
@@ -1,8 +1,27 @@
-//
-// Created by LesSugarR on 25-12-10.
-//
-
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
-
+#include<bits/stdc++.h>
+#include "Book.h"
+using namespace std;
+class Library;
+struct Date
+{
+	int year, month, day;
+	bool operator<(const Date& x) const;
+    string toString();
+};
+enum OpType
+{
+	ADD,
+	DELETE,
+	MODIFY
+};
+struct UndoRecord
+{
+	OpType op;
+	Book date;
+};
+bool PositiveInt(const string &s);
+void replace(string &s,const string &a,const string &b);
+string getTime();
 #endif //FUNCTIONS_H
